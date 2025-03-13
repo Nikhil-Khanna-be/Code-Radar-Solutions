3872c3df5229f17df81a4639dd946a268e363527
Nikhil-Khanna-be <nikhilkhanna1706@gmail.com>
2025-03-14 00:49:40 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int isprime(int);
+int isprime(int a){
+    if(a=1){
+        printf(0);
+    }
+    else if(a=2){
+        printf(1);
+    }
+    else{
+        for(i=2;i<a;i++){
+            if(a%i==0){
+                continue
+            }
+            else{
+                printf(1);
+                return 0;
+            }
+            printf(0);
+        }
+    }
+}
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int num;
+        scanf("%d",&num);
+        printf("%d\n",isprime(num));
+    }
+    return 0;
+}
