c68e1ccd6e85ab415b230422d535c181f0ab7f10
Nikhil-Khanna-be <nikhilkhanna1706@gmail.com>
2025-03-17 20:40:19 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,14 +2,16 @@
 
 int fibonacciSeries(int);
 int fibonacciSeries(int n){
-    if(n=1){
-        printf("%d ",0);
-    }
-    else if(n=2){
-        printf("%d ",1);
-    }
-    else{
-        printf("%d ",fibonacciSeries(n-1)+fibonacciSeries(n-2));
+    for(int i=1;i<=n;i++){
+        if(n=1){
+            printf("%d ",0);
+        }
+        else if(n=2){
+            printf("%d ",1);
+        }
+        else{
+            printf("%d ",fibonacciSeries(n-1)+fibonacciSeries(n-2));
+        }
     }
 }
 
