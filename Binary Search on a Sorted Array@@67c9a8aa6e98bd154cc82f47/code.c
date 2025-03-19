e6821eba0f4798babaa42984c95cbd7d1d64621e
Nikhil-Khanna-be <nikhilkhanna1706@gmail.com>
2025-03-20 01:48:42 +0530

    Solution to Binary Search on a Sorted Array

---
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -0,0 +1,9 @@
+int binarySearch(arr,n,target){
+    int n,arr[n],target;
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            print("%d",i);
+            break;
+        }
+    }
+}
