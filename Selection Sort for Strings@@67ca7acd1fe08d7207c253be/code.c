5dae672500e5d2c597fbb876de4b1ad3aedbd6b6
Nikhil-Khanna-be <nikhilkhanna1706@gmail.com>
2025-03-20 18:36:59 +0530

    Solution to Selection Sort for Strings

---
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -0,0 +1,15 @@
+void selectionSort(char arr[];int a){
+    for(int i=0;i<a-1;i++){
+        for(int j=0;j<a-i-1;j++){
+            if(arr[j][0]<arr[j+1][0]){
+                continue;
+            }
+            else{
+                 char temp[100]=arr[j];
+                 arr[j]=arr[j+1];
+                 arr[j+1]=arr[j];
+            }
+        }
+    }
+    
+}
